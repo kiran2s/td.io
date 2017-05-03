a6865e2814a541a5fdcc3ba8b82df9c24bc61970
kiran2s <kiranssivakumar@gmail.com>
2017-05-03 11:18:29 -0700

    Update TODO.

--- client/Client.js
+++ client/Client.js
@@ -32,7 +32,6 @@ class Client {
 		this.canvas = document.getElementById('canvas');
 		this.canvas.width = window.innerWidth;
 		this.canvas.height = window.innerHeight;
-		
 		this.ctx = this.canvas.getContext('2d');
 
 		if (this.gamestate === undefined) {
